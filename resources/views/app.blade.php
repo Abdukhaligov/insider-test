8a985ca427cef8f35b3874ddbfb0db80596d711d
Abdukhaligov <hikmat.pou@gmail.com>
2025-04-14 10:42:11 +0400

    temp: add assets to avoid building

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Insider Champions League</title>
-    @vite('resources/css/app.css')
+    <link href="{{ asset('app.css') }}" rel="stylesheet">
+    <link href="{{ asset('app-secondary.css') }}" rel="stylesheet">
 </head>
 
 <body>
     <div id="app"></div>
-    @vite('resources/js/app.js')
+    <script type="text/javascript" src="{{ asset('app.js') }}"></script>
 </body>
 
 </html>
